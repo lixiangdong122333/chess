0a606968f6e74e014a5944db6db99d92aac9088b
CMWL1231 <15940838353@163.com>
2023-09-18 23:43:28 +0800

    Changes

--- src/main/java/com/chess/BlackWidow.java
+++ src/main/java/com/chess/BlackWidow.java
@@ -1,10 +1,17 @@
 package com.chess;
 
 import com.chess.gui.Table;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@SpringBootApplication
 public class BlackWidow {
 
     public static void main(final String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(BlackWidow.class);
+        builder.headless(false).web(WebApplicationType.SERVLET).run(args);
         Table.get().show();
     }
 }
